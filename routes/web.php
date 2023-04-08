98a4abef1b752ac3df41bfdc1c0f320df726b24a
Gazi Akter <gaziakterpro@gmail.com>
2023-04-08 22:08:11 +0600

    load frontend template

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Home\HomeController;
 use App\Http\Controllers\SubCategoryController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,52 +19,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
- // Category All Route 
- Route::controller(CategoryController::class)->group(function () {
-    Route::get('/add/category', 'AddCategory')->name('add.category');
-    Route::get('/all/category', 'AllCategory')->name('all.category');
-    Route::get('/add/subcategory', 'AddSubCategory')->name('add.subcategory');
-    Route::get('/all/subcategory', 'AllSubCategory')->name('all.sub.category');
-    Route::post('/store/category', 'StoreCategory')->name('store.category');
-    Route::get('/edit/category/{id}','EditCategory')->name('edit.category');
-    Route::post('/update/category','UpdateCategory')->name('update.category');
-    Route::get('/delete/category/{id}','DeleteCategory')->name('delete.category');
-});
-
- // Sub Category All Route 
- Route::controller(SubCategoryController::class)->group(function () {
-    Route::get('/add/subcategory', 'AddSubCategory')->name('add.subcategory');
-    Route::post('/Add/subcategory', 'StoreSubCategory')->name('store.subcategory');
-    Route::get('/All/Subcategory','AllSubCategory')->name('all.sub.category');
-    Route::get('/Edit/Subcategory/{id}', 'EditSubcategory')->name('edit.subcategory');
-    Route::post('/update/sub/category','UpdateSubCategory')->name('update.sub.category');
-    Route::get('/delete/sub/category/{id}','DeleteSubCategory')->name('delete.subcategory');
-
+Route::controller(HomeController::class)->group(function(){
+    Route::get('/', 'HomePage')->name('home');
 });
 
 
- // Product All Route 
- Route::controller(ProductController::class)->group(function () {
-    Route::get('/add/product', 'AddProduct')->name('add.product');
-    Route::get('/all/product', 'AllProduct')->name('all.product');
-    Route::post('/store/product', 'StoreProduct')->name('store.product');
-    Route::get('/Edit/image/{id}', 'EditImage')->name('edit.image');
-    Route::post('/update/product/image', 'UpdateProductImage')->name('update.product.image');
-    Route::get('/Edit/product/{id}', 'EditProduct')->name('edit.product');
-    Route::post('/update/product', 'UpdateProduct')->name('update.product');
-    Route::get('/delete/product/{id}', 'DeleteProduct')->name('delete.product');
+// middleware use for auth
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
+    // Category All Route 
+    Route::controller(CategoryController::class)->group(function () {
+        Route::get('/add/category', 'AddCategory')->name('add.category');
+        Route::get('/all/category', 'AllCategory')->name('all.category');
+        Route::get('/add/subcategory', 'AddSubCategory')->name('add.subcategory');
+        Route::get('/all/subcategory', 'AllSubCategory')->name('all.sub.category');
+        Route::post('/store/category', 'StoreCategory')->name('store.category');
+        Route::get('/edit/category/{id}','EditCategory')->name('edit.category');
+        Route::post('/update/category','UpdateCategory')->name('update.category');
+        Route::get('/delete/category/{id}','DeleteCategory')->name('delete.category');
+    });
+
+    // Sub Category All Route 
+    Route::controller(SubCategoryController::class)->group(function () {
+        Route::get('/add/subcategory', 'AddSubCategory')->name('add.subcategory');
+        Route::post('/Add/subcategory', 'StoreSubCategory')->name('store.subcategory');
+        Route::get('/All/Subcategory','AllSubCategory')->name('all.sub.category');
+        Route::get('/Edit/Subcategory/{id}', 'EditSubcategory')->name('edit.subcategory');
+        Route::post('/update/sub/category','UpdateSubCategory')->name('update.sub.category');
+        Route::get('/delete/sub/category/{id}','DeleteSubCategory')->name('delete.subcategory');
+
+    });
+
+
+    // Product All Route 
+    Route::controller(ProductController::class)->group(function () {
+        Route::get('/add/product', 'AddProduct')->name('add.product');
+        Route::get('/all/product', 'AllProduct')->name('all.product');
+        Route::post('/store/product', 'StoreProduct')->name('store.product');
+        Route::get('/Edit/image/{id}', 'EditImage')->name('edit.image');
+        Route::post('/update/product/image', 'UpdateProductImage')->name('update.product.image');
+        Route::get('/Edit/product/{id}', 'EditProduct')->name('edit.product');
+        Route::post('/update/product', 'UpdateProduct')->name('update.product');
+        Route::get('/delete/product/{id}', 'DeleteProduct')->name('delete.product');
+    });
+
 });
-
-
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
